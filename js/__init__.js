8938ffac64364b8722a811e860304519fe079a0d
Asbjørn Enge <asbjorn@hanafjedle.net>
2013-01-26 00:12:36 +0100

    Started the new design

--- js/__init__.js
+++ js/__init__.js
@@ -1,3 +1,15 @@
 $(document).ready(function() {
 	$('h1').text("Call me to come here with a crooked crescendo, but i don't.");
+	$a.menu.bind();
 })
+
+$a = {}
+
+$a.menu = {}
+
+$a.menu.bind = function() {
+	$('#menu li').click(function() {
+		$('#menu li.selected').removeClass('selected');
+		$(this).addClass('selected');
+	})
+}
