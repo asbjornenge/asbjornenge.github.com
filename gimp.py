909f5affd69ece4d1abd9461bec5cc82b2099a22
Asbjørn Enge <asbjorn@hanafjedle.net>
2013-01-26 01:30:37 +0100

    Some more gimp work

--- gimp.py
+++ gimp.py
@@ -1,14 +1,36 @@
-import sys, json
+import sys, os, json
 
-
-available_actions = ['build']
 def read_config():
-	return open('gimp.json').read()
+    return json.loads(open('gimp.json').read())
+def save_config(config):
+    open('gimp.json','w').write(json.dumps(config))
+def add_blog(config):
+    name = len(sys.argv) > 2 and sys.argv[2] or ""
+    if name == "":
+        print "You have to specify blog name"
+        sys.exit(1)
+    if not os.path.exists(name):
+        os.makedirs(name)
+        config['blogs'].append(name)
+def build(config):
+    # first build an object graph
+    # os.walk('.') to find .mako files and render
+    return
+def build_blog(config):
+    # for each blog look for markdown files and render with 
+    # inherit=settings.blogparent OR main.blogparent
+    return
+
+action_map = {
+    'build' : build,
+    'add'   : add_blog
+}
 
 if __name__ == "__main__":
-	action = len(sys.argv) > 1 and sys.argv[1] or ""
-	if action not in available_actions:
-		print "Unknown action <%s>" % action
-		sys.exit(1)
-	config = read_config()
-	print config
+    action = len(sys.argv) > 1 and sys.argv[1] or ""
+    if action not in action_map.keys():
+        print "Unknown action <%s>" % action
+        sys.exit(1)
+    config = read_config()
+    action_map[action](config)
+    save_config(config)
