fc8b9ce708af9ec5986516b66a90cff6c04bd255
Asbjørn Enge <asbjorn@hanafjedle.net>
2013-01-27 23:53:58 +0100

    Working on load indicator

--- js/init.js
+++ js/init.js
@@ -1,25 +1,57 @@
 $(document).ready(function() {
 	// $a.menu.bind();
-	switch(window.location.pathname) {
+
+	move_nav_button(window.location.pathname);
+
+	$('a').click(function(e) {
+		e.preventDefault();
+		move_nav_button($(this).attr('href'));
+		// window.location = $(this).attr('href');
+	})
+
+})
+
+var current_xhr;
+
+function move_nav_button(path) {
+	load_page(path)
+	switch(path) {
 		case '/about.html':
-			$('#menu .about').addClass('selected');
+			position_nav_button($('#menu .about'))
 			break;
 		case '/crly.html':
-			$('#menu .crly').addClass('selected');
+			position_nav_button($('#menu .crly'))
 			break;
 		case '/software.html':
-			$('#menu .software').addClass('selected');
+			position_nav_button($('#menu .software'))
 			break;
 		case '/':
-			$('#menu .blog').addClass('selected');
+			position_nav_button($('#menu .blog'))
 			break;
 		default:
 			console.log('Unknown path '+window.location.pathname);
 	}
-	if (tap_event === 'tap') {
-		$('a').click(function(e) {
-			e.preventDefault();
-			window.location = $(this).attr('href');
-		})
-	}
-})
+}
+
+function position_nav_button(selected) {
+	var pos   = selected.position();
+	var width = selected.width();
+	var left  = pos.left + (width/2);
+	$('#head .nav_selector').css('left',left);
+}
+
+function load_page(path) {
+	if (current_xhr != null) current_xhr.abort();
+	$('#head .nav_selector').addClass('loading');
+	current_xhr = $.get(path, function(data) {
+		setTimeout(function() {
+			$('#head .nav_selector').removeClass('loading');
+		}, 450);
+		var nc = $(data).find('.push').parent();
+		$("#content").html(nc);
+		current_xhr = null;
+		if (window.history != undefined)
+			history.pushState(null, null, path)
+	})
+	console.log(current_xhr);
+}
